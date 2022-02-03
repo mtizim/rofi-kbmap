2305c52bb72f8f60dac53f5101dd8708c4877599
Mateusz Ziemła <34897665+mtizim@users.noreply.github.com>
2022-02-03 23:14:38 +0100

    Handle not choosing anything correctly

--- rofi-kbmap.py
+++ rofi-kbmap.py
@@ -7,13 +7,16 @@ LAUNCH_STR = "layout"
 
 
 # get available layouts
-options = "\n".join(
+options = (
     (next(x for x in os.popen("setxkbmap -query").read().split("\n") if "layout" in x))
     .split()[1]
     .split(",")
 )
-res = subprocess.getoutput('echo "' + options + f'" | rofi -i -dmenu -p {LAUNCH_STR}')
-langs = ",".join([res] + [option for option in options.split("\n") if option != res])
 
-# set res as the first choice
-os.popen(f"setxkbmap {langs}")
+res = subprocess.getoutput(
+    'echo "' + "\n".join(options) + f'" | rofi -i -dmenu -p {LAUNCH_STR}'
+)
+if any(res == option for option in options):
+    # set res as the first choice
+    langs = ",".join([res] + [option for option in options if option != res])
+    os.popen(f"setxkbmap {langs}")
